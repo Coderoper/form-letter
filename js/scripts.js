161909201510c4990e0bcc01524734b174191927
Azamat Bekmuratov and Austin Roberts <azamat@>
2018-03-21 10:49:26 -0700

    This will commit form-letter project

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#blank').submit(function(event) {
+    event.preventDefault();
+    var personInput = $("input#person").val();
+    $('.person').text(personInput);
+
+    $('#story').show();
+
+
+  });
+
+
+});
